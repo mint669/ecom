55adf1ca61c44e2e7d13ff7eb7aa8d5074a5b043
sdasda744 <sda167744@gmail.com>
2025-10-08 07:31:41 -0600

    feat service auth: implemented auth and midddle in services

---
+++ apps/client/src/app/test/page.tsx
@@ -0,0 +1,36 @@
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
+
+const TestPage = async () => {
+  const { getToken } = await auth();
+  const token = await getToken();
+
+  const resProduct = await fetch("http://localhost:8080/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataProduct = await resProduct.json();
+  console.log("dataProduct >>>", dataProduct);
+
+  const resOrder = await fetch("http://localhost:8081/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataOrder = await resOrder.json();
+  console.log("dataOrder >>>", dataOrder);
+
+
+  const resPayment = await fetch("http://localhost:8082/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataPayment = await resPayment.json();
+  console.log("dataPayment >>>", dataPayment);
+  
+  return <div>TestPage</div>;
+};
+
+export default TestPage;
--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -1,14 +1,25 @@
 import Fastify from "fastify";
+import { clerkPlugin } from "@clerk/fastify";
+import { shouldBeUser } from "./middleware/authMiddleware";
 
 const fastify = Fastify();
+
+fastify.register(clerkPlugin);
 
 fastify.get("/health", (request, reply) => {
   return reply.status(200).send({
     status: "OK",
     uptime: process.uptime(),
-    timestamp: Date.now()
-  })
-})
+    timestamp: Date.now(),
+  });
+});
+
+fastify.get("/test", { preHandler: shouldBeUser }, (request, reply) => {
+  return reply.send({
+    message: "Order service is authenticated.",
+    userId: request.userId,
+  });
+});
 
 const start = async () => {
   try {
